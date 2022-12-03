543ec167acae90b8da453a5e4647fdfa2abf012f
Lyr01 <79974123+Lyr01@users.noreply.github.com>
2022-12-03 10:42:32 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			{/* Advice #<!-- Advice ID goes here -->
+
+"<!-- Advice text goes here -->" */}
+
+			<div className="attribution">
+				Challenge by{" "}
+				<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+					Frontend Mentor
+				</a>
+				. Coded by <a href="#">Your Name Here</a>.
+			</div>
+		</div>
+	);
+}
+
+export default App;
