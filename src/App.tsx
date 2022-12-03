dbc8df13eb92886b8d0e2e0f9b0e2de5fd4fb906
Lyr01 <79974123+Lyr01@users.noreply.github.com>
2022-12-03 12:45:53 +0100

    completed css and html

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+interface Advice {
+	id: number;
+	advice: string;
+}
+
 function App() {
+	const [data, setData] = useState<Advice>();
+
+	const URL = "https://api.adviceslip.com/advice";
+
+	const handleFetch = () => {
+		fetch(URL)
+			.then((res) => res.json())
+			.then((data) => setData(data.slip));
+	};
+
+	useEffect(() => {
+		handleFetch();
+	}, []);
+
 	return (
-		<div className="App">
-			{/* Advice #<!-- Advice ID goes here -->
-
-"<!-- Advice text goes here -->" */}
-
-			<div className="attribution">
-				Challenge by{" "}
-				<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-					Frontend Mentor
-				</a>
-				. Coded by <a href="#">Your Name Here</a>.
+		<main className="App">
+			<div className="container">
+				<div className="advice-id">
+					<span>Advice {data?.id}</span>
+				</div>
+				<div className="advice-content">
+					<p>{data?.advice}</p>
+				</div>
+				<div className="divider"></div>
+				<div onClick={handleFetch} className="logo"></div>
 			</div>
-		</div>
+		</main>
 	);
 }
 
